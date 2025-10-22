1f2af0a113db5953b6fac9482dd4a172bc6f82c4
Victor Hugo Cretton Rivera <victorcrettonrivera@gmail.com>
2025-10-22 18:44:40 -0300

    Prueba: Prueba para github actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import './App.css'
+import "./App.css";
+
+export function Hello(_name: string) {
+  const unused = 123; // debería marcar warning por no usado
+  return `Hola`;
+}
 
 function App() {
-
   return (
     <>
       <div>
-           <h1>HolaMundo('print')</h1>
+        <h1>HolaMundo('print')</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
