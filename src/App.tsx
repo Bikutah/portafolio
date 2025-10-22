88592ee6a91e2b341b52ce6b3d475c6230317c73
Victor Hugo Cretton Rivera <victorcrettonrivera@gmail.com>
2025-10-22 18:59:28 -0300

    Arreglada la funcionalidad

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import "./App.css";
 
-export function Hello(_name: string) {
-  const unused = 123; // debería marcar warning por no usado
-  return `Hola`;
-}
-
 function App() {
   return (
     <>
       <div>
-        <h1>HolaMundo('print')</h1>
+        <h1>HolaMundo('printa')</h1>
       </div>
     </>
   );
